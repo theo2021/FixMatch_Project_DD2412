241bc1ec5f9ffbd1fd02f301dcd50ec5d0d50722
Frederico Nesti <fredericonesti@gmail.com>
2020-10-13 23:24:57 +0200

    first commit dataloader

---
+++ data/data.py
@@ -0,0 +1,132 @@
+import os
+import argparse
+import torchvision
+from tfds.features import FeaturesDict
+import tensorflow as tf
+import torch
+from torch.utils.data import DataLoader
+from torchvision.datasets import ImageFolder
+from torch.utils import data
+from PIL import Image
+
+parser = argparse.ArgumentParser(description='Dataset')
+
+parser.add_argument('--download', type=bool, default=False)
+parser.add_argument('--root', type=str, default=os.getcwd())
+parser.add_argument('--use_database', type=str, default='CIFAR10')
+parser.add_argument('--task', type=str, default='train')
+parser.add_argument('--batchsize', type=int, default=2)
+
+args = parser.parse_args()
+
+
+#https://www.cs.toronto.edu/~kriz/cifar.html
+#http://ufldl.stanford.edu/housenumbers/
+
+if args.use_database == 'CIFAR10':
+
+        database_dict = FeaturesDict({
+                                'id': Text(shape=(), dtype=tf.string),
+                                'image': Image(shape=(32, 32, 3), dtype=tf.uint8),
+                                'label': ClassLabel(shape=(), dtype=tf.int64, num_classes=10)
+                           })
+
+	if args.task == 'train':
+		database = torchvision.datasets.CIFAR10(args.root, train=True, transform=None, target_transform=None, download=args.download)
+
+	else if args.task == 'test':
+		database = torchvision.datasets.CIFAR10(args.root, train=False, transform=None, target_transform=None, download=args.download)
+
+	else:
+		print('  ')
+
+else if args.use_database == 'CIFAR100':
+
+        database_dict = FeaturesDict({
+                                'id': Text(shape=(), dtype=tf.string),
+                                'image': Image(shape=(32, 32, 3), dtype=tf.uint8),
+                                'label': ClassLabel(shape=(), dtype=tf.int64, num_classes=100)
+                           })
+
+        if args.task == 'train':
+                database = torchvision.datasets.CIFAR100(args.root, train=True, transform=None, target_transform=None, download=args.download)
+
+        else if args.task == 'test':
+                database = torchvision.datasets.CIFAR100(args.root, train=False, transform=None, target_transform=None, download=args.download)
+
+	else:
+		print(' ')
+
+else if args.use_database == 'SVHN':
+
+        database_dict = FeaturesDict({
+                                'id': Text(shape=(), dtype=tf.string),
+                                'image': Image(shape=(32, 32, 3), dtype=tf.uint8),
+                                'label': ClassLabel(shape=(), dtype=tf.int64, num_classes=10)
+                           })
+
+        if args.task == 'train':
+                database = torchvision.datasets.SVHN(args.root, split='train', transform=None, target_transform=None, download=args.download)
+
+
+        else if args.task == 'test':
+                database = torchvision.datasets.SVHN(args.root, split='test', transform=None, target_transform=None, download=args.download)
+
+	else:
+		print(' ')
+
+else:
+	print(' ')
+
+print(database.__getitem__(0))
+
+'''
+weak_augmentation = torchvision.transforms.Compose([
+        torchvision.transforms.RandomHorizontalFlip(),
+        torchvision.transforms.RandomAffine(0, translate=(0.1, 0.1), shear=10, scale=(0.85, 1.15), fillcolor=0),
+        # TransformShow(), # visualize transformed pic
+        torchvision.transforms.ToTensor(),
+    ])
+
+strong_augmentation  = transforms.Compose([
+        #transforms.Resize(32),  # rescale the image keeping the original aspect ratio
+        #transforms.CenterCrop(32),  # we get only the center of that rescaled
+        #transforms.RandomCrop(32),  # random crop within the center crop (data augmentation)
+        transforms.ColorJitter(brightness=(0.9, 1.1)),
+        transforms.RandomRotation((-10, 10)),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomAffine(0, translate=(0.1, 0.1), shear=10, scale=(0.85, 1.15), fillcolor=0),
+        # TransformShow(), # visualize transformed pic
+        transforms.ToTensor()
+    ])
+
+
+class Augmented_Dataset(data.Dataset):
+	def __init__(self, db, db_dict, transform=None, forget_labels=0.0)
+		super().__init__()
+		self.db = db
+		self.db_dict = db_dict
+		self.transform = transform
+		self.forget_labels = forget_labels
+		
+	def __getitem__(self, index):
+	'Generates one sample of data'
+
+		#PIL loading
+		im_pil = image.resize(self.db[index][0])
+
+		if self.transform:
+			X = self.transform(im_pil)
+		else:
+			transformer_tf = tf.ToTensor()
+			X = transformer_tf(im_pil)
+
+			y = self.db[index][1]
+
+		return X, y
+
+
+#train_loader = DataLoader(database, batch_size=args.batchsize, shuffle=True, pin_memory=False, num_workers=2)
+
+
+'''	
