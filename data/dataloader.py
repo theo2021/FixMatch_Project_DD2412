2f1b90263f4870a6779c87ed6d89c5cefa785cfe
Frederico Nesti <fredericonesti@gmail.com>
2020-10-13 11:18:57 +0200

    Commit test

--- data/dataloader.py
+++ data/dataloader.py
@@ -1,24 +1,33 @@
 import torch
-#from tfds.features import FeaturesDict
+from tfds.features import FeaturesDict
 
-
+'''
+def activation_func(activation):
+    return  torch.nn.ModuleDict([
+                            ['relu', nn.ReLU(inplace=True)],
+                            ['leaky_relu', nn.LeakyReLU(negative_slope=0.01, inplace=True)],
+                            ['selu', nn.SELU(inplace=True)],
+                            ['none', nn.Identity()]
+                        ])[activation]
+'''
 
 class Set_Dataset(torch.utils.data.Dataset):
 
 	def __init__(self, desired_dataset):
 		if desired_dataset == 'CIFAR-10':
-			'''
-			Dataset_Dict = FeaturesDict({
+			
+			self.Dataset_Dict = FeaturesDict({
 							'id': Text(shape=(), dtype=tf.string),
 							'image': Image(shape=(32, 32, 3), dtype=tf.uint8),
 							'label': ClassLabel(shape=(), dtype=tf.int64, num_classes=10)
 						   })
 			'''
 			Dataset_Dict = torch.nn.ModuleDict({
-								'id': Text(shape=(), dtype=tf.string),
-                                                        	'image': Image(shape=(32, 32, 3), dtype=tf.uint8),
-                                                        	'label': ClassLabel(shape=(), dtype=tf.int64, num_classes=10)
+								'id': Text(shape=(), dtype=string),
+                                                        	'image': Image(shape=(32, 32, 3), dtype=uint8),
+                                                        	'label': ClassLabel(shape=(), dtype=int64, num_classes=10)
 							  }, modules=iterable)
+			'''
 		if else desired_dataset == 'CIFAR-100':
 			print('not implemented yet')
 
