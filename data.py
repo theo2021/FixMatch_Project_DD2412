1666c425a2bf2e805d27dc6109bdb4db454f0956
Frederico Nesti <47975100+FredericoNesti@users.noreply.github.com>
2020-10-16 15:41:22 +0200

    Update data.py

--- data.py
+++ data.py
@@ -1,13 +1,14 @@
 import os
 import argparse
 import torchvision
-from tfds.features import FeaturesDict
+#from tfds.features import FeaturesDict
 import tensorflow as tf
 import torch
 from torch.utils.data import DataLoader
 from torchvision.datasets import ImageFolder
 from torch.utils import data
 from PIL import Image
+from torchvision import transforms
 
 parser = argparse.ArgumentParser(description='Dataset')
 
@@ -54,6 +55,7 @@ print(database.__getitem__(0))
 '''
 
 class Dataloader(torch.utils.data.Dataset):
+
 	def __init__(self, download = True, db_name = 'CIFAR10', db_dir = None, mode = 'train'):
 		self.transform = None
 		self.db_name = db_name
@@ -63,52 +65,55 @@ class Dataloader(torch.utils.data.Dataset):
 		self.download = download
 
 	def loader(self):
-
+		print('inside loader')
 		if self.db_name == 'CIFAR10':
 
-		        if self.mode == 'train':
-		                self.database = torchvision.datasets.CIFAR10(self.db_dir, train=True, transform=None, target_transform=None, download=self.download)
+			if self.mode == 'train':
+				self.database = torchvision.datasets.CIFAR10(self.db_dir, train=True, transform=None, target_transform=None, download=self.download)
 
-		        elif self.mode == 'test':
-		                self.database = torchvision.datasets.CIFAR10(self.db_dir, train=False, transform=None, target_transform=None, download=self.download)
+			elif self.mode == 'test':
+				self.database = torchvision.datasets.CIFAR10(self.db_dir, train=False, transform=None, target_transform=None, download=self.download)
 
 		elif self.db_name == 'CIFAR100':
 
-		        if self.mode == 'train':
-		                self.database = torchvision.datasets.CIFAR100(self.db_dir, train=True, transform=None, target_transform=None, download=self.download)
+			if self.mode == 'train':
+				self.database = torchvision.datasets.CIFAR100(self.db_dir, train=True, transform=None, target_transform=None, download=self.download)
 
-		        elif self.mode == 'test':
-		                self.database = torchvision.datasets.CIFAR100(self.db_dir, train=False, transform=None, target_transform=None, download=self.download)
+			elif self.mode == 'test':
+				self.database = torchvision.datasets.CIFAR100(self.db_dir, train=False, transform=None, target_transform=None, download=self.download)
 
 		elif self.db_name == 'SVHN':
 
-		        if self.mode == 'train':
-		                self.database = torchvision.datasets.SVHN(self.db_dir, split='train', transform=None, target_transform=None, download=self.download)
+			if self.mode == 'train':
+				self.database = torchvision.datasets.SVHN(self.db_dir, split='train', transform=None, target_transform=None, download=self.download)
 
-		        elif self.mode == 'test':
-		                self.database = torchvision.datasets.SVHN(self.db_dir, split='test', transform=None, target_transform=None, download=self.download)
+			elif self.mode == 'test':
+				self.database = torchvision.datasets.SVHN(self.db_dir, split='test', transform=None, target_transform=None, download=self.download)
 
 
 
 
 	def __getitem__(self, index):
-		self.databse = self.loader[index]
+
+		print('inside get item')
+
+		self.database = self.loader[index]
 
 		print('debug')
 		print(self.database.shape)
 
-	        im_pil = self.database[0]
+		im_pil = self.database[0]
 		im_label = self.database[1]
 
-	        if self.transform:
-	            X = self.transform(im_pil)
-	        else:
-	            transformer_tf = tf.ToTensor()
-	            X = transformer_tf(im_pil)
+		if self.transform:
+			X = self.transform(im_pil)
+		else:
+			transformer_tf = tf.ToTensor()
+			X = transformer_tf(im_pil)
 
-	        y = torch.tensor(im_label, dtype=torch.long)
+		y = torch.tensor(im_label, dtype=torch.long)
 
-	        return X, y
+		return X, y
 
 class Augmentation(Dataloader):
 	def __init__(self, aug_type = 'weak'):
@@ -117,34 +122,29 @@ class Augmentation(Dataloader):
 
 	def weak_augment(self, visualize = False):
 
+		print('inside augmentation')
+
 		if visualize == True:
-			self.transform = = torchvision.transforms.Compose([
+				self.transform = torchvision.transforms.Compose([
 					        torchvision.transforms.RandomHorizontalFlip(),
 					        torchvision.transforms.RandomAffine(0, translate=(0.1, 0.1), shear=10, scale=(0.85, 1.15), fillcolor=0),
-						TransformShow(),
+							transforms.TransformShow(),
 					        torchvision.transforms.ToTensor(),
 					    ])
 		else:
-                        self.transform = = torchvision.transforms.Compose([
-                                                torchvision.transforms.RandomHorizontalFlip(),
-                                                torchvision.transforms.RandomAffine(0, translate=(0.1, 0.1), shear=10, scale=(0.85, 1.15), fillcolor=0),
-                                                torchvision.transforms.ToTensor(),
-                                            ])
+				self.transform = torchvision.transforms.Compose([
+										torchvision.transforms.RandomHorizontalFlip(),
+										torchvision.transforms.RandomAffine(0, translate=(0.1, 0.1), shear=10, scale=(0.85, 1.15), fillcolor=0),
+										torchvision.transforms.ToTensor(),
+									])
 
 
-	def rand_augment(self, visualize = False):
+	#def rand_augment(self, visualize = False):
 
-	def ctaugment(self, visualize = False):
+	#def ctaugment(self, visualize = False):
 
 
 '''
-weak_augmentation = torchvision.transforms.Compose([
-        torchvision.transforms.RandomHorizontalFlip(),
-        torchvision.transforms.RandomAffine(0, translate=(0.1, 0.1), shear=10, scale=(0.85, 1.15), fillcolor=0),
-        # TransformShow(), # visualize transformed pic
-        torchvision.transforms.ToTensor(),
-    ])
-
 strong_augmentation  = transforms.Compose([
         #transforms.Resize(32),  # rescale the image keeping the original aspect ratio
         #transforms.CenterCrop(32),  # we get only the center of that rescaled
@@ -156,39 +156,11 @@ strong_augmentation  = transforms.Compose([
         # TransformShow(), # visualize transformed pic
         transforms.ToTensor()
     ])
-
-
-class Augmented_Dataset(data.Dataset):
-	def __init__(self, db, db_dict, transform=None, forget_labels=0.0)
-		super().__init__()
-		self.db = db
-		self.db_dict = db_dict
-		self.transform = transform
-		self.forget_labels = forget_labels
-
-	def __getitem__(self, index):
-	'Generates one sample of data'
-
-		#PIL loading
-		im_pil = image.resize(self.db[index][0])
-
-		if self.transform:
-			X = self.transform(im_pil)
-		else:
-			transformer_tf = tf.ToTensor()
-			X = transformer_tf(im_pil)
-
-			y = self.db[index][1]
-
-		return X, y
-
-
+    
 #train_loader = DataLoader(database, batch_size=args.batchsize, shuffle=True, pin_memory=False, num_workers=2)
-
-
 '''
 
 
-augmented_dataset = Augmentation().weak_augment(visualize = True, db_dir = args.root, db_name = args.use_database, mode = args.task, download = args.download)
+augmented_dataset = Augmentation(db_dir = args.root, db_name = args.use_database, mode = args.task, download = args.download).weak_augment(visualize = True)
 
 print(augmented_dataset.__getitem__(0))
