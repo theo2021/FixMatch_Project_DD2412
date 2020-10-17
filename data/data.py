7028428c3c2ab065bdc853bec7689c8d7820919a
kristmundur <kristmunduragust@gmail.com>
2020-10-17 13:46:02 +0000

    CTAugment demo

--- data/data.py
+++ data/data.py
@@ -12,6 +12,7 @@ import cv2, random, functools
 from collections import defaultdict
 from math import ceil
 import numpy as np
+from ctaugment import CTAugment, apply
 
 parser = argparse.ArgumentParser(description='Dataset')
 
@@ -27,7 +28,25 @@ args = parser.parse_args()
 #https://www.cs.toronto.edu/~kriz/cifar.html
 #http://ufldl.stanford.edu/housenumbers/
 
+class Augmentation:
+    def __init__(self):
+        self.cta = CTAugment()
+        self.weak = self.__weak()
 
+    def strong(self, x):
+        policy = self.cta.policy(True)
+
+        return (apply(x, policy), policy)
+
+    def update(self, policy, accuracy):
+        self.cta.update_rates(policy, accuracy)
+
+    def __weak(self):
+        transforms = torchvision.transforms.Compose([torchvision.transforms.RandomHorizontalFlip(p=0.5),
+                     torchvision.transforms.RandomAffine(0, translate=(0.125, 0.125)),
+                     torchvision.transforms.ToTensor()])
+
+        return transforms
 class DataLoader:
     def __init__(self, dataset, B, mu, num_classes, K):
         self.dataset      = dataset
@@ -116,10 +135,10 @@ class DataSet(torch.utils.data.Dataset):
 
 
         #transformer_tf = tf.ToTensor()
-        X =  torchvision.transforms.ToTensor()(im_pil)
+        #X =  torchvision.transforms.ToTensor()(im_pil)
      
-        y = torch.tensor(im_label, dtype=torch.long)
-
+        #y = torch.tensor(im_label, dtype=torch.long)
+        X, y = im_pil, im_label
         return X, y, index in self.labeled_set
 
     def __len__(self):
@@ -136,7 +155,7 @@ class DataSet(torch.utils.data.Dataset):
 
 
 
-
+'''
 class Augmentation(DataSet):
     def __init__(self,  labels_per_class, download = True, db_name = 'CIFAR10', db_dir = None, mode = 'train', aug_type = 'weak'):
         super().__init__(download, db_name, db_dir, mode)
@@ -151,6 +170,12 @@ class Augmentation(DataSet):
     def __getitem__(self, i):
         X, y, has_label = self.database[i]#super().__getitem__(i)
         return self.weak_augment()(X), y, i in self.labeled_set
+'''
+
+def visualize(img):
+    X = cv2.cvtColor(cv2.resize(img.transpose(1,2,0), (256, 256)), cv2.COLOR_BGR2RGB)
+    cv2.imshow('t', X)
+    cv2.waitKey()
 
 if __name__ == "__main__":
    
@@ -164,5 +189,15 @@ if __name__ == "__main__":
 
     for (ub, b, lbl) in loader:
         print(len(ub), len(b), len(lbl))
+
+
+    
+    im = dataset[0][0]
+    augmentation = Augmentation()
+
+    visualize(augmentation.weak(im).numpy())
+    visualize(augmentation.strong(np.array(im))[0])
+    
+
 
     #print(augmented_dataset.__getitem__(0))
