76b30d31ae666cb3c31823987ffa8ae87932b1fb
theo2021 <theo2021@gmail.com>
2020-10-22 10:30:15 +0200

    adding warmup

--- scheduler/cosineLRreduce.py
+++ scheduler/cosineLRreduce.py
@@ -4,13 +4,22 @@ import math
 
 class cosineLRreduce(_LRScheduler):
 
-    def __init__(self, optimizer, K):
+    def __init__(self, optimizer, K, warmup=None):
         self.optimizer = optimizer
         self.K = K
+        self.warm = 1.0*K/100 if warmup is None else warmup # default 1/100 of total training
+        self.state = 0 # indicates state, 0 init, 1 warmup, 2 cosine
         super(cosineLRreduce, self).__init__(optimizer)
 
     def get_lr(self):
-        if self._step_count - 2 > self.K:
+        c_step = self._step_count -2
+        if c_step > self.K:
             print("scheduler error, k > K")
-        weight = math.cos(7 * math.pi / 16 / self.K * (self._step_count-2))
+            return [base_lr/(2**10) for base_lr in self.base_lrs] # if error return very small learning rate
+        elif c_step < self.warm:
+            weight = c_step / self.warm
+            self.state = 1
+        else:
+            weight = math.cos(7.0 * math.pi / 16 / self.K * (c_step - self.warm))
+            self.state = 2
         return [base_lr*weight for base_lr in self.base_lrs]
